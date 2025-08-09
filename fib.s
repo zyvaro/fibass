5f3a42962a4a30168ecd703351c9b80f70f5db6c
dedNewGuy <abeamin23@gmail.com>
2025-08-09 12:39:30 +0800

    print hello world

--- fib.s
+++ fib.s
@@ -1,6 +1,16 @@
 .global _start
 
 _start:
+	mov $1, %rax
+	mov $1, %rdi
+	mov $hello, %rsi
+	mov $hello_len, %rdx
+	syscall
+
 	mov $60, %rax
 	mov $0, %rdi
 	syscall
+
+hello:
+	.ascii "hello, world\n"
+hello_len = . - hello
