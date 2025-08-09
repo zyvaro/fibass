ac78e7936e7dcced992b6e24656ad40baf535e4d
dedNewGuy <abeamin23@gmail.com>
2025-08-09 15:10:25 +0800

    print 1 digit with a buffer_size

--- fib.s
+++ fib.s
@@ -8,12 +8,15 @@ _start:
 	mov x(%rip), %rbx
 	add $48, %rbx
 	mov %rbx, buffer
+	addl $1, buffer_size
 	movq $10, buffer + 1
+	addl $1, buffer_size
+	movq $0, buffer + 2
 
 	mov $1, %rax
 	mov $1, %rdi
 	lea buffer, %rsi
-	mov $2, %rdx
+	lea buffer_size, %rdx
 	syscall
 
 	mov $60, %rax
