89c5e66f18d3f6357ceb35667794cb2f0f4aafe2
dedNewGuy <abeamin23@gmail.com>
2025-08-10 13:16:08 +0800

    print integer in correct order. Very nice!

--- fib.s
+++ fib.s
@@ -3,6 +3,9 @@ _start:
 	mov $0, %r15
 	mov num, %r12
 	lea buffer(%rip), %r11
+	add $255, %r11
+	movb $10, (%r11)
+	addl $1, buffer_len
 	jmp .L2
 .L3:
 	mov %r12, %rax
@@ -13,22 +16,19 @@ _start:
 	div %rcx
 	mov %rax, %r12
 	add $48, %rdx
-	mov %rdx, (%r11)
+	dec %r11
+	movb %dl, (%r11)
 	addl $1, buffer_len
-	add $1, %r11
 	add $1, %r15
 .L2:
 	cmp $9, %r15
 	jle .L3
 .L4:
 
-	add $1, %r11
-	movl $10, (%r11)
-
 	mov $1, %rax
 	mov $1, %rdi
-	lea buffer(%rip), %rsi
-	lea buffer_len(%rip), %rdx
+	mov %r11, %rsi
+	mov buffer_len, %rdx
 	syscall
 
 	mov $60, %rax
@@ -41,4 +41,4 @@ buffer_len: .space 4
 
 .section .data
 hello: .string "hello, world\n"
-num: .long 123
+num: .long 4589
