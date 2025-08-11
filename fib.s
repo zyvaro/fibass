2923732a3ca970ab42899eeba0c88f0cfa092131
dedNewGuy <abeamin23@gmail.com>
2025-08-11 14:41:20 +0800

    we have a function to print positive number now

--- fib.s
+++ fib.s
@@ -7,9 +7,10 @@ print_number:
 	movl -4(%rbp), %eax
 	movl $0, -4(%rbp) # Local counter variable
 	subq $256, %rsp # reserve 256 bytes above -8(%rbp) for buffer
-	leaq -256(%rbp), %r12
+	leaq -260(%rbp), %r12
+	addq $255, %r12
 	movb $10, (%r12) # add new line to the buffer
-	inc %r12
+	dec %r12
 	addl $1, -4(%rbp) # Increase counter
 	movl $10, %ecx
 .L0:
@@ -18,14 +19,14 @@ print_number:
 	div %ecx 
 	addl $48, %edx
 	movb %dl, (%r12) # Store the converted number to ascii into buffer
-	inc %r12
+	dec %r12
 	addl $1, -4(%rbp) # Increase counter
 	cmp $0, %eax
 	jg .L0
 .L1:
 	movq $1, %rax
 	movq $1, %rdi
-	leaq -4(%r12), %rsi 
+	leaq 1(%r12), %rsi 
 	movq -4(%rbp), %rdx
 	syscall
 	addq $256, %rsp
@@ -34,7 +35,7 @@ print_number:
 
 .global _start
 _start:
-	movl $123, %edi
+	movl $4096, %edi
 	call print_number
 
 	mov $60, %rax
